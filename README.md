5e3697d3155713b0d3bcc3286709d4db2e863d6f
ykonno <konchan1129@gmail.com>
2017-09-15 14:08:12 +0100

    initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# いつもやってる設定
+
+### いつものアプリDependency
+* なんにせよ入れる
+  * actuator
+  * lombok
+  * devtools
+* Web系なら入れる
+  * web
+  * security
+  * mybatis
+  * Thymeleaf
---
+++ src/main/java/pink/nico/boottmpl/BootTmplApplication.java
@@ -0,0 +1,12 @@
+package pink.nico.boottmpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTmplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTmplApplication.class, args);
+	}
+}
